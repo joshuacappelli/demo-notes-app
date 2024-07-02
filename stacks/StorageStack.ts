d5f9b5d9f40235099e085e626222df6b09c23bfa
joshuacappelli <joshua@cappelliconnect.com>
2024-07-02 18:55:48 -0400

    enabling CORS

--- stacks/StorageStack.ts
+++ stacks/StorageStack.ts
@@ -2,7 +2,16 @@ import { Bucket, StackContext, Table } from "sst/constructs";
 
 export function StorageStack({ stack }: StackContext) {
   // Create the DynamoDB table
-  const bucket = new Bucket(stack, "uploads");
+  const bucket = new Bucket(stack, "Uploads", {
+    cors: [
+      {
+        maxAge: "1 day",
+        allowedOrigins: ["*"],
+        allowedHeaders: ["*"],
+        allowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+      },
+    ],
+  });
   const table = new Table(stack, "Notes", {
     fields: {
       userId: "string",
